570ff1b9eb93b63ba6145faca9eda49d2ff5d135
Mauro Polenta <maurop4502@gmail.com>
2024-03-16 19:12:40 -0300

    Versión mobile first

---
+++ js/main.js
@@ -0,0 +1,28 @@
+let htmlElement = document.getElementById('htmlText');
+let jsonElement = document.getElementById('jsonText');
+
+document.getElementById('convertBtn').addEventListener('click', function() {
+  if (htmlElement.value != '') {
+    let lines = htmlElement.value.split('\n'); // Split the input by newlines directly
+    let jsonLines = lines.map(line => JSON.stringify(line)); // Convert each line to a JSON string if needed
+    jsonElement.value = '[\n  ' + jsonLines.join(',\n  ') + '\n]'; // Create a JSON array representation
+  } 
+});
+
+document.getElementById('copy').addEventListener('click', function() {
+  jsonElement.select();
+  navigator.clipboard.writeText(jsonElement.value)
+        .then(() => {
+            const span = document.getElementsByTagName('span')[0];
+            const img = document.getElementsByTagName('img')[0];
+            span.textContent = 'Copied!';
+            img.src = './img/check.svg';
+            setTimeout(() => {
+                span.textContent = 'Copy code';
+                img.src = './img/copy.svg';
+            }, 2000);
+        })
+        .catch(err => {
+            console.error('Error in copying text: ', err);
+        });
+});
